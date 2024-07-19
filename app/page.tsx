30f961dcc39a3422ffaef17667d3514f0db63e92
r0leksii <rozhkoaleks@gmail.com>
2024-07-19 17:22:50 +0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,196 @@
-import Image from "next/image";
+import "dotenv/config";
+import { Card } from "@/components/ui/card";
+require("dotenv").config();
 
-export default function Home() {
+// Define the TypeScript types
+type MeasurementDetails = {
+  salePriceMeasurementSchemeItemId: number;
+  salePriceMeasurementUnitVolume: number;
+  salePriceFactor: number;
+  salePriceUnit: string;
+  costPriceMeasurementSchemeItemId: number;
+  costPriceMeasurementUnitVolume: number;
+  costPriceFactor: number;
+  costPriceUnit: string;
+};
+
+type Supplier = {
+  id: number;
+  name: string;
+  description: string;
+  addressLine1: string;
+  addressLine2: string;
+  town: string;
+  county: string;
+  postCode: string;
+  contactNumber: string;
+  contactNumber2: string;
+  emailAddress: string;
+  type: string;
+  referenceCode: string;
+};
+
+type TaxRate = {
+  taxGroupId: number;
+  taxRateId: number;
+  locationId: number;
+  priority: number;
+  percentage: number;
+  name: string;
+  description: string;
+  taxCode: string;
+};
+
+type TaxGroup = {
+  id: number;
+  name: string;
+  taxRates: TaxRate[];
+};
+
+type ProductTag = {
+  id: number;
+  name: string;
+};
+
+type ProductUdf = {
+  id: number;
+  name: string;
+  value: string;
+};
+
+type ProductLocationAreaPrice = {
+  locationAreaId: number;
+  salePrice: number;
+  costPriceExcTax: number;
+  eatOutPrice: number;
+};
+
+type ProductImage = {
+  productId: number;
+  productImageId: number;
+  imageUrl: string;
+  mainImage: boolean;
+};
+
+type CustomerProductPricingDetail = {
+  priceId: number;
+  typeId: number;
+  typeName: string;
+  price: number;
+  eatOutPrice: number;
+  productId: number;
+};
+
+type ProductDetail = {
+  productId: number;
+  detailedDescription: string;
+};
+
+type Product = {
+  Id: number;
+  Name: string;
+  Description: string;
+  costPrice: number;
+  isCostPriceIncTax: boolean;
+  SalePrice: number;
+  isSalePriceIncTax: boolean;
+  eatOutPrice: number;
+  isEatOutPriceIncTax: boolean;
+  categoryId: number;
+  barcode: string;
+  salePriceTaxGroupId: number;
+  eatOutPriceTaxGroupId: number;
+  costPriceTaxGroupId: number;
+  brandId: number;
+  supplierId: number;
+  popupNoteId: number;
+  unitOfSale: number;
+  volumeOfSale: number;
+  variantGroupId: number;
+  multipleChoiceNoteId: number;
+  size: string;
+  sku: string;
+  sellOnWeb: boolean;
+  sellOnTill: boolean;
+  orderCode: string;
+  sortPosition: number;
+  rrPrice: number;
+  productType: number;
+  tareWeight: number;
+  articleCode: string;
+  isTaxExemptable: boolean;
+  referenceCode: string;
+  isVariablePrice: boolean;
+  excludeFromLoyaltyPointsGain: boolean;
+  IsArchived: boolean;
+  colourId: number;
+  measurementDetails: MeasurementDetails;
+  supplier: Supplier;
+  salePriceTaxGroup: TaxGroup;
+  eatOutPriceTaxGroup: TaxGroup;
+  costPriceTaxGroup: TaxGroup;
+  productTags: ProductTag[];
+  productUdfs: ProductUdf[];
+  additionalSuppliersIds: number[];
+  productLocationAreaPrices: ProductLocationAreaPrice[];
+  productImages: ProductImage[];
+  isMultipleChoiceProductOptional: boolean;
+  customerProductPricingDetails: CustomerProductPricingDetail[];
+  containerFeeId: number;
+  buttonColourId: number;
+  productDetails: ProductDetail;
+};
+
+const NEXT_EPOS_KEY = process.env.NEXT_EPOS_KEY;
+const NEXT_EPOS_SECRET = process.env.NEXT_EPOS_SECRET;
+const credentials = `${NEXT_EPOS_KEY}:${NEXT_EPOS_SECRET}`;
+const base64Credentials = btoa(credentials);
+
+async function getCategories(): Promise<Product[]> {
+  const res = await fetch("https://api.eposnowhq.com/api/v4/category/", {
+    cache: "force-cache",
+    headers: {
+      Authorization: `Basic ${base64Credentials}`,
+    },
+  });
+
+  return await res.json();
+}
+
+async function getProducts(): Promise<Product[]> {
+  const res = await fetch("https://api.eposnowhq.com/api/v4/product/", {
+    cache: "force-cache",
+    headers: {
+      Authorization: `Basic ${base64Credentials}`,
+    },
+  });
+
+  return await res.json();
+}
+
+export default async function Home() {
+  const categories = await getCategories();
+  const products = await getProducts();
+  const filteredProducts = products
+    .filter((product) => !product.IsArchived)
+    .slice(0, 100);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-col gap-3 p-4">
+      <div className="grid grid-cols-6 gap-3">
+        {categories.map((category) => (
+          <div key={category.Id}>{category.Name}</div>
+        ))}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="grid grid-cols-6 gap-3">
+        {filteredProducts.map((product) => (
+          <Card className={"p-4"} key={product.Id}>
+            <h2>{product.Name}</h2>
+            <p>{product.Description}</p>
+            <p>Sale Price: ${product.SalePrice}</p>
+            <p>ID: {product.Id}</p>
+          </Card>
+        ))}
       </div>
     </main>
   );
